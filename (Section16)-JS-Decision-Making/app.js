db87febea96390fa57d1e48eb2488501b08072e9
Ethan Coulter <ethanscottcoulter@gmail.com>
2023-01-17 15:49:12 -0500

    Finished section 16

---
+++ (Section16)-JS-Decision-Making/app.js
@@ -0,0 +1,15 @@
+console.group("Eh yo whats good errbody")
+
+let total = 1 + 4 + 6
+let x = Math.random(1,20)
+console.log(total, "Goodbye")
+
+if (x <= 10 ) {
+    console.log("number was less than or equal to 10")
+}else {
+    console.log("Number was greater than 10")
+}
+
+if (1 + 1 === 2) {
+    console.log("LETS GOOOOOOOOO")
+}
